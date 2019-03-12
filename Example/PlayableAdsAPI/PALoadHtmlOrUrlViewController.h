e96ed3043d34a0a0c4da6dcebdd40cb1cd25d89b
JiaDingYi <wzy2010416033@163.com>
2019-03-12 18:52:46 +0800

    layout

---
+++ Example/PlayableAdsAPI/PALoadHtmlOrUrlViewController.h
@@ -0,0 +1,17 @@
+//
+//  PALoadHtmlOrUrlViewController.h
+//  PlayableAdsAPI_Example
+//
+//  Created by 王泽永 on 2019/3/12.
+//  Copyright © 2019 wzy2010416033@163.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PALoadHtmlOrUrlViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/PlayableAdsAPI/PALoadHtmlOrUrlViewController.m
@@ -0,0 +1,32 @@
+//
+//  PALoadHtmlOrUrlViewController.m
+//  PlayableAdsAPI_Example
+//
+//  Created by 王泽永 on 2019/3/12.
+//  Copyright © 2019 wzy2010416033@163.com. All rights reserved.
+//
+
+#import "PALoadHtmlOrUrlViewController.h"
+
+@interface PALoadHtmlOrUrlViewController ()
+
+@end
+
+@implementation PALoadHtmlOrUrlViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
