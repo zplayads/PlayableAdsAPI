74fb6e0a246eaf2764d22fa5085db251484d5d6b
JiaDingYi <wzy2010416033@163.com>
2019-03-13 09:53:00 +0800

    add render vc

---
+++ Example/PlayableAdsAPI/PARenderViewController.h
@@ -0,0 +1,17 @@
+//
+//  PARenderViewController.h
+//  PlayableAdsAPI_Example
+//
+//  Created by 王泽永 on 2019/3/12.
+//  Copyright © 2019 wzy2010416033@163.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PARenderViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/PlayableAdsAPI/PARenderViewController.m
@@ -0,0 +1,32 @@
+//
+//  PARenderViewController.m
+//  PlayableAdsAPI_Example
+//
+//  Created by 王泽永 on 2019/3/12.
+//  Copyright © 2019 wzy2010416033@163.com. All rights reserved.
+//
+
+#import "PARenderViewController.h"
+
+@interface PARenderViewController ()
+
+@end
+
+@implementation PARenderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
