e3f9c59dc78bf9ec62e28f1143de211f1ce6aea8
ShunZhi Tang <tangshunzhi@zplay.com>
2019-03-25 18:41:47 +0800

    Add unified render protocol

---
+++ Example/PlayableAdsAPI/RenderWebView/PAUnifiedRenderWebView.h
@@ -0,0 +1,28 @@
+//
+//  PAUnifiedRenderWebView.h
+//  PlayableAdsAPI_Example
+//
+//  Created by Michael Tang on 2019/3/25.
+//  Copyright © 2019 wzy2010416033@163.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@protocol PARenderVcDelegate <NSObject>
+
+- (void)PARenderVcDidClosed;
+
+@end
+
+@protocol PAUnifiedRenderWebView <NSObject>
+
+- (void)setAdModel:(PAAdsModel *)adModel;
+- (void)setFunctionType:(SupportFunctionType)functionType;
+- (void)setDelegate:(id<PARenderVcDelegate> _Nullable)delegate;
+- (void)setLoadUrl:(NSString *)urlString;
+- (void)loadHTMLString:(NSString *)htmlStr isReplace:(BOOL)isReplace;
+
+@end
+
+NS_ASSUME_NONNULL_END
