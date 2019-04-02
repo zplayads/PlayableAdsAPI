38ad5f38471a6ced8547ba37148a1780d6c27e34
ShunZhi Tang <tangshunzhi@zplay.com>
2019-04-02 19:54:08 +0800

    Add isSupportMraid_02

--- Example/PlayableAdsAPI/Tools/PAHeaderFile.h
+++ Example/PlayableAdsAPI/Tools/PAHeaderFile.h
@@ -29,4 +29,5 @@ typedef enum : NSUInteger {
     SettingValueIdentify_LoadHTMLorURL_01,
     SettingValueIdentify_PreRender_02,
     SettingValueIdentify_LoadHTMLorURL_02,
+    SettingValueIdentify_SupportMraid_02,
 } SettingValueIdentify;
